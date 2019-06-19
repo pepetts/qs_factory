b395c1d03e901684bc2ed576508abe570116a54c
pepetts <pepepetts@gmail.com>
2019-06-19 10:15:41 +0900

    タイマーのスタートボタンの2度押し防止

--- main.js
+++ main.js
@@ -29,20 +29,28 @@ $(function(){
     $('#min').text(minInner);
   }
 
+
   var timer_obj = $('#timer');
+
+  //タイマースタートボタン
   $('#start_btn').click(function(){
-    time = setInterval(timer,100);
+    time = setInterval(timer,1000);
+    $('#start_btn').addClass('disable');
   });
 
+  //ストップボタン
   $('#stop_btn').click(function(){
     clearInterval(time);
+    $('#start_btn').removeClass('disable');
   });
 
+  //リセットボタン
   $('#reset_btn').click(function(){
     min = 0;
     sec = 0;
     clearInterval(time);
     $('#min,#sec').text("00");
+    $('#start_btn').removeClass('disable');
   });
 
 })
